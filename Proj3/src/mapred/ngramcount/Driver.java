e19c006ce5e8ae5f23d9c59a96b87f14a3b914cf
Huaiming Yang <huaimingyang@huaimingdembp.wv.cc.cmu.edu>
2015-03-26 18:41:24 -0400

    repair ngram

--- Proj3/src/mapred/ngramcount/Driver.java
+++ Proj3/src/mapred/ngramcount/Driver.java
@@ -8,25 +8,21 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 
 public class Driver {
-
+	public static int n = 1;
 	public static void main(String args[]) throws Exception {
 		SimpleParser parser = new SimpleParser(args);
 
 		String input = parser.get("input");
 		String output = parser.get("output");
-		// Added by chenw-0314-2014
-		int N = parser.getInt("n");
-
-		getJobFeatureVector(input, output, N);
+		n = Integer.valueOf(parser.get("n"));
+		getJobFeatureVector(input, output);
 
 	}
 
-	private static void getJobFeatureVector(String input, String output, int N)
+	private static void getJobFeatureVector(String input, String output)
 			throws IOException, ClassNotFoundException, InterruptedException {
-		Configuration conf = new Configuration();
-		conf.set("n", Integer.toString(N));
-		Optimizedjob job = new Optimizedjob(conf, input, output,
-				"Compute NGram Count ");
+		Optimizedjob job = new Optimizedjob(new Configuration(), input, output,
+				"Compute NGram Count");
 
 		job.setClasses(NgramCountMapper.class, NgramCountReducer.class, null);
 		job.setMapOutputClasses(Text.class, NullWritable.class);
--- Proj3/src/mapred/ngramcount/NgramCountMapper.java
+++ Proj3/src/mapred/ngramcount/NgramCountMapper.java
@@ -1,5 +1,5 @@
 package mapred.ngramcount;
-import java.io.*;
+
 import java.io.IOException;
 
 import mapred.util.Tokenizer;
@@ -8,7 +8,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.conf.Configuration;
 
 public class NgramCountMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
 
@@ -17,21 +16,14 @@ public class NgramCountMapper extends Mapper<LongWritable, Text, Text, NullWrita
 			throws IOException, InterruptedException {
 		String line = value.toString();
 		String[] words = Tokenizer.tokenize(line);
-
-		//for (String word : words)
-		//	context.write(new Text(word), NullWritable.get());
-
-		Configuration conf = context.getConfiguration();
-		int N = Integer.parseInt(conf.get("n"));
-		for (int i = 0; i < words.length - (N - 1); i ++)
-		{
-			String keyString = words[i];
-			for (int j = 1; j < N; j ++)
-			{
-				keyString = keyString + " " + words[i + j];
+		int l = words.length;
+		for (int i = 0; i < l - Driver.n + 1; i++) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(words[i]);
+			for (int j = i + 1; j < Driver.n; j++) {
+				sb.append(" " + words[j]);
 			}
-			context.write(new Text(keyString), NullWritable.get());
+			context.write(new Text(sb.toString()), NullWritable.get());
 		}
-
 	}
 }

10ea2ba561cd746d0516fd947e52053f74d19732
Pan Sun <sunpan9209@gmail.com>
2015-04-15 07:57:29 -0400

    pagerank hadoop v1

--- PageRank/Hadoop/src/mapred/pagerank/XmlInputFormat.java
+++ PageRank/Hadoop/src/mapred/pagerank/XmlInputFormat.java
@@ -17,7 +17,7 @@
 
 package mapred.pagerank;
 
-import org.apache.hadoop.conf.Configuration;
+import java.io.IOException;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -30,115 +30,111 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
-import java.io.IOException;
-
 /**
- * Reads records that are delimited by a specific begin/end tag.
+ * Reads records that are delimited by a specifc begin/end tag.
  */
 public class XmlInputFormat extends TextInputFormat {
 
-    public static final String START_TAG_KEY = "xmlinput.start";
-    public static final String END_TAG_KEY = "xmlinput.end";
+	public static final String START_TAG_KEY = "xmlinput.start";
+	public static final String END_TAG_KEY = "xmlinput.end";
 
-    @Override
-    public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) {
-        try {
-            return new XmlRecordReader((FileSplit) split, context);
-        } catch (IOException e) {
-            throw new RuntimeException("TODO : refactor this...");
-        }
-    }
+	@Override
+	public RecordReader<LongWritable, Text> createRecordReader(InputSplit is,
+			TaskAttemptContext tac) {
+		return new XmlRecordReader();
+	}
 
-    /**
-     * XMLRecordReader class to read through a given xml document to output xml
-     * blocks as records as specified by the start tag and end tag
-     */
-    public static class XmlRecordReader extends RecordReader<LongWritable, Text> {
-        private final byte[] startTag;
-        private final byte[] endTag;
-        private final long start;
-        private final long end;
-        private final FSDataInputStream fsin;
-        private final DataOutputBuffer buffer = new DataOutputBuffer();
+	public static class XmlRecordReader extends
+			RecordReader<LongWritable, Text> {
+		private byte[] startTag;
+		private byte[] endTag;
+		private long start;
+		private long end;
+		private FSDataInputStream fsin;
+		private DataOutputBuffer buffer = new DataOutputBuffer();
+		private LongWritable key = new LongWritable();
+		private Text value = new Text();
 
-        private LongWritable key = new LongWritable();
-        private Text value = new Text();
+		@Override
+		public void initialize(InputSplit is, TaskAttemptContext tac)
+				throws IOException, InterruptedException {
+			FileSplit fileSplit = (FileSplit) is;
+			startTag = tac.getConfiguration().get(START_TAG_KEY)
+					.getBytes("utf-8");
+			endTag = tac.getConfiguration().get(END_TAG_KEY).getBytes("utf-8");
+			start = fileSplit.getStart();
+			end = start + fileSplit.getLength();
+			Path file = fileSplit.getPath();
 
+			FileSystem fs = file.getFileSystem(tac.getConfiguration());
+			fsin = fs.open(fileSplit.getPath());
+			fsin.seek(start);
+		}
 
-        public XmlRecordReader(FileSplit split, TaskAttemptContext context) throws IOException {
-            Configuration conf = context.getConfiguration();
-            startTag = conf.get(START_TAG_KEY).getBytes("utf-8");
-            endTag = conf.get(END_TAG_KEY).getBytes("utf-8");
+		@Override
+		public boolean nextKeyValue() throws IOException, InterruptedException {
+			if (fsin.getPos() < end) {
+				if (readUntilMatch(startTag, false)) {
+					try {
+						buffer.write(startTag);
+						if (readUntilMatch(endTag, true)) {
+							value.set(buffer.getData(), 0, buffer.getLength());
+							key.set(fsin.getPos());
+							return true;
+						}
+					} finally {
+						buffer.reset();
+					}
+				}
+			}
+			return false;
+		}
 
-            // open the file and seek to the start of the split
-            start = split.getStart();
-            end = start + split.getLength();
-            Path file = split.getPath();
-            FileSystem fs = file.getFileSystem(conf);
-            fsin = fs.open(split.getPath());
-            fsin.seek(start);
-        }
+		@Override
+		public LongWritable getCurrentKey() throws IOException,
+				InterruptedException {
+			return key;
+		}
 
-        @Override
-        public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
-        }
+		@Override
+		public Text getCurrentValue() throws IOException, InterruptedException {
+			return value;
 
-        @Override
-        public boolean nextKeyValue() throws IOException, InterruptedException {
-            if (fsin.getPos() < end) {
-                if (readUntilMatch(startTag, false)) {
-                    try {
-                        buffer.write(startTag);
-                        if (readUntilMatch(endTag, true)) {
-                            key.set(fsin.getPos());
-                            value.set(buffer.getData(), 0, buffer.getLength());
-                            return true;
-                        }
-                    } finally {
-                        buffer.reset();
-                    }
-                }
-            }
-            return false;
-        }
+		}
 
-        @Override
-        public LongWritable getCurrentKey() throws IOException, InterruptedException {
-            return key;
-        }
+		@Override
+		public float getProgress() throws IOException, InterruptedException {
+			return (fsin.getPos() - start) / (float) (end - start);
+		}
 
-        @Override
-        public Text getCurrentValue() throws IOException, InterruptedException {
-            return value;
-        }
+		@Override
+		public void close() throws IOException {
+			fsin.close();
+		}
 
-        @Override
-        public void close() throws IOException {
-            fsin.close();
-        }
+		private boolean readUntilMatch(byte[] match, boolean withinBlock)
+				throws IOException {
+			int i = 0;
+			while (true) {
+				int b = fsin.read();
+				// end of file:
+				if (b == -1)
+					return false;
+				// save to buffer:
+				if (withinBlock)
+					buffer.write(b);
+				// check if we're matching:
+				if (b == match[i]) {
+					i++;
+					if (i >= match.length)
+						return true;
+				} else
+					i = 0;
+				// see if we've passed the stop point:
+				if (!withinBlock && i == 0 && fsin.getPos() >= end)
+					return false;
+			}
+		}
+	}
 
-        @Override
-        public float getProgress() throws IOException {
-            return (fsin.getPos() - start) / (float) (end - start);
-        }
-
-        private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
-            int i = 0;
-            while (true) {
-                int b = fsin.read();
-                // end of file:
-                if (b == -1) return false;
-                // save to buffer:
-                if (withinBlock) buffer.write(b);
-
-                // check if we're matching:
-                if (b == match[i]) {
-                    i++;
-                    if (i >= match.length) return true;
-                } else i = 0;
-                // see if we've passed the stop point:
-                if (!withinBlock && i == 0 && fsin.getPos() >= end) return false;
-            }
-        }
-    }
 }
